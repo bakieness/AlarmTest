84a93b980137db55eae2d488a1885e24a4ee19aa
bakieness <BAKJ5_11@uni.worc.ac.uk>
2014-05-12 23:08:51 +0100

    test

--- WWW/js/index.js
+++ WWW/js/index.js
@@ -1,1 +1,37 @@
 // JavaScript Document
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function SetData()
+{
+	var name = document.getElementById("name").value;	
+	var age = document.getElementById("age").value;	
+	var gender = document.getElementById("gender").value;
+	
+	window.localStorage.setItem("name", name);	
+	window.localStorage.setItem("age", age);
+	window.localStorage.setItem("gender", gender);
+}
+
+function onDeviceReady()
+{
+	var db = window.openDatabase("myTestDB", "1.0", "myDb", 1024 * 1024 * 500);	
+	
+	db.transaction(function(tx) 
+	{
+		tx.executeSql('CREATE TABLE ALARMS (name, age, gender)');
+	});
+}
+
+function AddAlarms()
+{
+	var db = window.openDatabase("myTestDB", "1.0", "myDb", 1024 * 1024 * 500);
+	
+	var name = window.localStorage.getItem("name");	
+	var age = window.localStorage.getItem("age");
+	var gender = window.localStorage.getItem("gender");
+		
+	db.transaction(function(tx) 
+	{
+		tx.executeSql('INSERT INTO ALARMS (name, age, gender) VALUES (?, ?, ?)', [name, age, gender]);
+	});
+}
